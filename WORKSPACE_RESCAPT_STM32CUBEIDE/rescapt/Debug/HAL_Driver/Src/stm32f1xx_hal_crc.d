e60948a7b030aeb333e224edfcd0f9b95e9e5f61
d3choubr <d3choubr@enib.fr>
2024-10-23 23:07:36 +0200

    Re-add WORKSPACE_RESCAPT_STM32CUBEIDE to the repository

---
+++ WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/Debug/HAL_Driver/Src/stm32f1xx_hal_crc.d
@@ -0,0 +1,103 @@
+HAL_Driver/Src/stm32f1xx_hal_crc.o: ../HAL_Driver/Src/stm32f1xx_hal_crc.c \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_conf.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/device/stm32f1xx.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/device/stm32f103xb.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/core/core_cm3.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/core/core_cmInstr.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/core/cmsis_gcc.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/core/core_cmFunc.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/device/system_stm32f1xx.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_eth.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_can.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_can_ex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_cec.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_adc.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_crc.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_dac.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_sram.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_ll_fsmc.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_nor.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_i2s.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_iwdg.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_rtc.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_rtc_ex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_pccard.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_sd.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_nand.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_spi.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_uart.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_usart.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_irda.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_smartcard.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_wwdg.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_ll_usb.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_hcd.h
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_conf.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_def.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/device/stm32f1xx.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/device/stm32f103xb.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/core/core_cm3.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/core/core_cmInstr.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/core/cmsis_gcc.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/core/core_cmFunc.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/CMSIS/device/system_stm32f1xx.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_eth.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_can.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_can_ex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_cec.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_adc.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_crc.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_dac.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_sram.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_ll_fsmc.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_nor.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_i2s.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_iwdg.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_rtc.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_rtc_ex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_pccard.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_sd.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_nand.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_spi.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_uart.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_usart.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_irda.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_smartcard.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_wwdg.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_pcd.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_ll_usb.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/HAL_Driver/Inc/stm32f1xx_hal_hcd.h:
---
+++ WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/Debug/src/MadgwickAHRS.d
@@ -0,0 +1,3 @@
+src/MadgwickAHRS.o: ../src/MadgwickAHRS.c \
+ /home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/inc/MadgwickAHRS.h
+/home/douae/ENIB/CRS/WORKSPACE_RESCAPT_STM32CUBEIDE/rescapt/inc/MadgwickAHRS.h:
